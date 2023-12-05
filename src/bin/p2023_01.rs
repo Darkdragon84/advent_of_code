9ec054ce8cab464701bf2afd8b88e2dc3f773a67
Valentin <v.stauber@parityqc.com>
2023-12-05 15:49:53 +0100

    delete commented out code

--- src/bin/p2023_01.rs
+++ src/bin/p2023_01.rs
@@ -8,23 +8,14 @@ const DIG_STR: &str = "one|two|three|four|five|six|seven|eight|nine";
 fn get_str_to_digit() -> HashMap<&'static str, &'static str> {
     HashMap::from([
         ("one", "1"),
-        // ("1", 1),
         ("two", "2"),
-        // ("2", 2),
         ("three", "3"),
-        // ("3", 3),
         ("four", "4"),
-        // ("4", 4),
         ("five", "5"),
-        // ("5", 5),
         ("six", "6"),
-        // ("6", 6),
         ("seven", "7"),
-        // ("7", 7),
         ("eight", "8"),
-        // ("8", 8),
         ("nine", "9"),
-        // ("9", 9),
     ])
 }
 
